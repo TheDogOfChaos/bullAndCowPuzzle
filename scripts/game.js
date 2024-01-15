1ab223e9a8931338fe1db0d3b2447800a3699e12
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-01-15 13:27:05 +0000

    oh shit i forgor about this

--- scripts/game.js
+++ scripts/game.js
@@ -3,21 +3,33 @@ $(document).ready(function(){
 		let urlParams = new URLSearchParams(window.location.search)
 		return urlParams.get(value) 
 	}
+	
+	function getMaxIntForBits(bits) {
+		return 2 ** bits;
+	}
 
 	let difficulty = getUrlParamVal('diff');
-	let switchVals= [];
+	let switchVals = [];
+	let targetVal = Math.floor(Math.random() * (2 ** difficulty)) + 1;
 	
 	for (let i = 0; i < difficulty; i++) {
+		if (((i % 8) == 0) & !(i == 0)) {
+			$('.switchbox').append('<br>');
+		}
 		$('.switchbox').append(function(n){
-			let switchID = 'switch' + (i + 1);
-			let switchCreate = $('<input type="checkbox" class="switch">');
-			switchCreate.attr('id', switchID);
+			let switchCreate = $('<input type="button" class="switch">');
+			switchCreate.attr('id', i + 1);
+			switchCreate.attr('value', i + 1);
 			return switchCreate;
 		});
+		console.log(((i % 8) == 0).toString(2) + i)
+
 	}
 	
+	
+	
 	$('#checkAnswer').click(function() {
-		let  checkboxes = $('.switchbox').find('.switch');
+		let checkboxes = $('.switchbox').find('.switch');
 		$('.switch').each(function(index, element) {
 			if ($(element).prop('checked')) {
 				let switchID = element.id;
@@ -28,8 +40,9 @@ $(document).ready(function(){
 		console.log('Checked Checkbox Values:', switchVals);
 	});
 	
-	//TODO: Make randomising array to check against
-	//TODO: Limited chances to check?
-	//TODO: Resident Evil 2 reserve power puzzle-esque?
+	//NOPE: Make randomising array to check against
+	//TODO: Assign each checkbox a binary value
+	//IDEA: Limited chances to check?
+	//IDEA: Resident Evil 2 reserve power puzzle-esque?
 	
 }); 
