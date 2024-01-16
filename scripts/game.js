5304e8891725639237b24a171010fc0b78643079
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-01-16 15:52:47 +0000

    i think i fixed it

--- scripts/game.js
+++ scripts/game.js
@@ -11,6 +11,8 @@ $(document).ready(function(){
 	let difficulty = getUrlParamVal('diff');
 	let guess = [];
 	let secret = []
+	let bull = 0;
+	let cow = 0;
 	let rand;
 	
 	for (let i = 0; i < difficulty; i++) {
@@ -18,25 +20,31 @@ $(document).ready(function(){
 			rand = Math.floor(Math.random() * 10);
 		}
 		while (secret.includes(rand));
-		secret.push(rand);
+		secret.push(parseInt(rand, 10));
 	}
 	$('.numButton').click(function() {
 		if (guess.length != difficulty){
 			$('#answer').append(this.id)
-			guess.push(this.id)
+			guess.push(parseInt(this.id, 10))
 		}
 	});
 	
 	$('#checkAnswer').click(function() {
 		console.log(secret)
+		bull = 0; cow = 0;
 		for (let i=0; i < difficulty; i++) {
 			if (guess[i] == secret[i]) {
 				console.log("bull")
-			} else if (secret.includes(guess[i])) {
+				bull++
+			} else if (secret.includes(guess[i]) && guess[i] != secret[i]) {
 				console.log("cow")
-			} else {
-				console.log("no u")
+				cow++
 			}
+		}
+		$("#bulls").text(bull);
+		$("#cows").text(cow);
+		if (bull == difficulty) {
+			console.log("You win!")
 		}
 	});
 	
@@ -48,6 +56,5 @@ $(document).ready(function(){
 	//NOPE: Make randomising array to check against
 	//TODO: Assign each checkbox a binary value
 	//IDEA: Limited chances to check?
-	//IDEA: Resident Evil 2 reserve power puzzle-esque?
 	
 }); 
